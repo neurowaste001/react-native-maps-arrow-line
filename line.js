ebc6880ee117420fcc0b4445861fecdbc89537bf
neurowaste001 <81636168+neurowaste001@users.noreply.github.com>
2023-09-22 11:29:09 +0200

    Update line.js

--- line.js
+++ line.js
@@ -4,7 +4,7 @@ import DefaultArrow from "./default";
 import calculateRotation from './calc';
 import headingContext from './context';
 
-const ArrowedPolyline = ({arrow = null, addOnlyLastArrow = false,  samplingRate=15, arrowSize=8, arrowColor=null, ...polylineProps}) => {
+const ArrowedPolyline = ({arrow = null, addOnlyLastArrow = false, arrowColor=null, arrowSize = 8, ...polylineProps}) => {
   const {
     coordinates = [],
     geodesic = false,
@@ -16,9 +16,13 @@ const ArrowedPolyline = ({arrow = null, addOnlyLastArrow = false,  samplingRate=
   const markerData = useMemo(() => {
      if(addOnlyLastArrow){
        const index = coordinates.length - 1;
-       return [calculateRotation(coordinates[index], coordinates[index - 1], geodesic, heading)]
+       return [calculateRotation(coordinates[index], coordinates[index - 1], geodesic, heading.heading)]
       }
-     const result = coordinates.reduce((acc, coord, index) => {if (index%samplingRate===0){acc.push(calculateRotation(coord, coordinates[index - 1], geodesic, heading))}; return acc}, []);
+     const zoom = Math.floor(heading.zoom);
+     const sampling = zoom<=5?610:zoom<=6?377:zoom<=7?233:zoom<=8?144:zoom<=9?89:zoom<=10?55:zoom<=11?34:zoom===12?21:zoom===13?13:zoom===14?8:zoom===15?5:zoom===16?3:1;
+     const delta = 360*Math.exp(-zoom*Math.LN2)*2;ƒ
+     const inScreen = (coord)=> heading?.center?.latitude+delta>coord.latitude && heading?.center?.latitude-delta<coord.latitude && heading?.center?.longitude+delta>coord.longitude && heading?.center?.longitude-delta<coord.longitude;
+     const result = coordinates.reduce((acc, coord, index) => {if (index%sampling===0 && inScreen(coord)){acc.push(calculateRotation(coord, coordinates[index - 1], geodesic, heading.heading))}; return acc}, []);
      // first item will be empty as we don't place marker at the line start
      result.shift();
      return result;
@@ -33,7 +37,7 @@ const ArrowedPolyline = ({arrow = null, addOnlyLastArrow = false,  samplingRate=
         const color = arrowColor || strokeColors[index % strokeColors.length] || strokeColor;
         return (
           <Marker {...markerProps} tappable={false} anchor={{ y: -0.01 }} tracksViewChanges={false} centerOffset={{y: -0.01}}>
-            <Component color={color} size={arrowSize} />
+            <Component color={color} size={arrowSize}   />
           </Marker>
         );
       })}
