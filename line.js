db7ba5869abb8704ad824351261481dab409962d
neurowaste001 <81636168+neurowaste001@users.noreply.github.com>
2023-09-22 11:32:01 +0200

    Update line.js

--- line.js
+++ line.js
@@ -20,7 +20,7 @@ const ArrowedPolyline = ({arrow = null, addOnlyLastArrow = false, arrowColor=nul
       }
      const zoom = Math.floor(heading.zoom);
      const sampling = zoom<=5?610:zoom<=6?377:zoom<=7?233:zoom<=8?144:zoom<=9?89:zoom<=10?55:zoom<=11?34:zoom===12?21:zoom===13?13:zoom===14?8:zoom===15?5:zoom===16?3:1;
-     const delta = 360*Math.exp(-zoom*Math.LN2)*2;ƒ
+     const delta = 360*Math.exp(-zoom*Math.LN2)*2;
      const inScreen = (coord)=> heading?.center?.latitude+delta>coord.latitude && heading?.center?.latitude-delta<coord.latitude && heading?.center?.longitude+delta>coord.longitude && heading?.center?.longitude-delta<coord.longitude;
      const result = coordinates.reduce((acc, coord, index) => {if (index%sampling===0 && inScreen(coord)){acc.push(calculateRotation(coord, coordinates[index - 1], geodesic, heading.heading))}; return acc}, []);
      // first item will be empty as we don't place marker at the line start
